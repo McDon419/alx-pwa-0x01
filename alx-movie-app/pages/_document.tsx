d341e861320ebc2d3d8a50143e4068ac37ab19df
McDon419 <ebenezerdonkor890@gmail.com>
2025-09-26 21:18:48 +0000

    added icons

--- alx-movie-app/pages/_document.tsx
+++ alx-movie-app/pages/_document.tsx
@@ -3,8 +3,11 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
-      <body className="antialiased">
+      <Head>
+        <link rel="manifest" href="/manifest.json" />
+        <meta name="theme-color" content="#FFFFFF" />
+      </Head>
+      <body>
         <Main />
         <NextScript />
       </body>
